caac52d662f87b3778519b498bd5ac86ce510aac
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2019-02-10 02:36:16 -0200

    Life Life, the Universe, and Everything

---
+++ LifeTheUniverseAndEverything/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LifeTheUniverseAndEverything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            while(true){
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number == 42){
+                    break;
+                }
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
