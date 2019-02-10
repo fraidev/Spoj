235088e35f1486e104739ec97831e7f78376dc44
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2019-02-10 02:22:00 -0200

    Soma De Sequencia

---
+++ SomaDeSequencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SomaDaSequencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+
+            int soma = 0;
+            for(int i = 0; i < Convert.ToInt32(n); i++){
+                var x = Console.ReadLine();
+                soma += Convert.ToInt32(x);
+            }         
+            Console.WriteLine(soma);
+        }
+    }
+}
